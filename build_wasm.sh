bcf1c456088f9cdf5c724a4deb5b3ab247c758d6
Stefan Negru <stefan@blankdots.com>
2024-04-24 09:34:15 +0300

    refactor build_wasm script for shell

--- build_wasm.sh
+++ build_wasm.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-EMCC_CFLAGS="-I/emsdk/upstream/include -L/emsdk/upstream/lib -sINITIAL_MEMORY=26214400" EMCC_FORCE_STDLIBS="libc emmake make" $1
+export EMCC_CFLAGS="-I/emsdk/upstream/include -L/emsdk/upstream/lib -sINITIAL_MEMORY=26214400" 
+export EMCC_FORCE_STDLIBS="libc" 
+
+emmake make $1
